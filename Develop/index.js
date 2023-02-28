b231b2e84446a2613d42c10110ed6d42d44eb05f
whitneywishart <whitneywishart@gmail.com>
2023-02-27 21:46:25 -0800

    refactor

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,86 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+inquirer
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'What is the description of your project?',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'How do users install your project?',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'How do users use your project?',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'How do users contribute to your project?',
+    name: 'contribute',
+  },
+  {
+    type: 'input',
+    message: 'How do users test your project?',
+    name: 'test',
+  },
+  {
+    type: 'checkbox',
+    message: 'What is the appropriate license for your project?',
+    name: 'license',
+    choices: ['None', 'MIT License', 'other'],
+  },
+  {
+    type: 'input',
+    message: 'What is your GitHub user name?',
+    name: 'username',
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'What should be included in your Table of Contents',
+    name: 'toc',
+  },
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success, lookit yer readme");
+  })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then(function (userInput) {
+      console.log(userInput)
+      writeToFile("README.md", generateMarkdown(userInput));
+    });
+}
 
-// Function call to initialize app
+// Function to initialize app
 init();
+
+
